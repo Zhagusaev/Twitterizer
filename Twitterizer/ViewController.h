727a30d5cf9438b023a5a073e16011d87da5fbd6
Yemi Ajibola <ya9p8@mst.edu>
2016-01-14 01:09:42 -0600

    Finished all components of assignment.

--- Twitterizer/ViewController.h
+++ Twitterizer/ViewController.h
@@ -10,6 +10,6 @@
 
 @interface ViewController : UIViewController
 
-
+-(NSMutableString*)addHashtags:(NSMutableString *) originalString;
 @end
 
--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -27,12 +27,47 @@ int counter =0;
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(NSMutableString*)addHashtags:(NSMutableString *) originalString
+{
+    NSArray* arrayofText = [originalString componentsSeparatedByString:@" "];
+    NSMutableString* hashtaggedString = [NSMutableString new];
+    NSMutableString* reverseString = [NSMutableString new];
+    for(int i=0; i < arrayofText.count; i++)
+    {
+        if(i%2 != 0) // even
+        {
+            NSString* value = (NSString *) [arrayofText objectAtIndex:i];
+            NSInteger length = value.length;
+            while (arrayofText[i] && length > 0)
+            {
+                length--;
+                NSRange subStrRange = NSMakeRange(length, 1);
+                [reverseString appendString:[arrayofText[i] substringWithRange:subStrRange]];
+            }
+            
+            //NSLog(@"Reverse string is: %@", reverseString);
+            [hashtaggedString appendString:reverseString];
+            [reverseString setString: @""];
+        }
+        else // odd
+        {
+            [hashtaggedString appendString:[@"#" stringByAppendingString:arrayofText[i]]];
+        }
+        [hashtaggedString appendString:@" "];
+        //NSLog(@"%@", hashtaggedString);
+    
+        
+    }
+
+    return hashtaggedString;
+}
+
 - (IBAction)onTwitterizeButtonPressed:(UIButton *)sender
 {
     NSString* initialString = self.textView.text;
     NSMutableString* twitterizedString = [NSMutableString new];
 
-//    NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
     NSString *vowels = @"AEIOUaeiou";
     
     for(int i = 0; i < initialString.length; i++)
@@ -44,8 +79,14 @@ int counter =0;
             [twitterizedString appendString:converted];
         }
     }
+    
+    twitterizedString = [self addHashtags:twitterizedString];
     self.textView.text = twitterizedString;
 }
+
+
+
+
 
 -(void)textViewDidChange:(UITextView *)textView
 {
@@ -55,16 +96,10 @@ int counter =0;
     self.counterLabel.text = [NSString stringWithFormat:@"%lu",(unsigned long)length];
 }
 
--(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
     return textView.text.length + (text.length - range.length) <= 140;
     
 }
 
--(void)textViewDidEndEditing:(UITextView *)textView
-{
-    for(int i=0; i < textView.text.length; i+2)
-    {
-        
-    }
-}
 @end
